e3be48673c4ed6cb56cdc53f0733f98d180cf809
jaem1n207 <roy.jm.lee@gmail.com>
2023-09-05 03:09:19 +0900

    :construction: 루트 경로 페이지 개발 중

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,29 @@
+import { redirect } from 'next/navigation';
+
+import { InitialModal } from '@/components/modals/initial-modal';
+import { db } from '@/lib/db';
+import { initialProfile } from '@/lib/initial-profile';
+
+export default async function SetupPage() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+
+  return (
+    <div>
+      <InitialModal />
+    </div>
+  );
+}

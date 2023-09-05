d12c4a1e51096fbd5d3c557ceb94d7b14c0b473a
jaem1n207 <roy.jm.lee@gmail.com>
2023-09-05 15:02:16 +0900

    :sparkles: Toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { Open_Sans } from 'next/font/google';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
@@ -23,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
           </ThemeProvider>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>

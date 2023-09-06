de3c7f279f228b22b6c5a8ff103ccf5f53cf5080
jaem1n207 <roy.jm.lee@gmail.com>
2023-09-06 16:09:11 +0900

    :sparkles: Layout

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -6,14 +6,12 @@ import ActionTooltip from '@/components/action-tooltip';
 
 export default function NavigationAction() {
   return (
-    <div>
-      <ActionTooltip side="right" label="서버 추가하기">
-        <button className="flex items-center group">
-          <div className="flex items-center justify-center mx-3 overflow-hidden transition-all h-48pxr w-48pxr rounded-24pxr group-hover:rounded-16pxr bg-background-primary group-hover:bg-emerald-500">
-            <Plus className="transition group-hover:text-white text-emerald-500" size={25} />
-          </div>
-        </button>
-      </ActionTooltip>
-    </div>
+    <ActionTooltip side="right" content={<p className="text-base font-semibold">서버 추가하기</p>}>
+      <button className="flex items-center group">
+        <div className="flex items-center justify-center mx-3 overflow-hidden transition-all h-48pxr w-48pxr rounded-24pxr group-hover:rounded-16pxr bg-background-primary group-hover:bg-emerald-500">
+          <Plus className="transition group-hover:text-white text-emerald-500" size={25} />
+        </div>
+      </button>
+    </ActionTooltip>
   );
 }

5de037b399236cc79b6bbd5b337807ad71a31c64
jaem1n207 <roy.jm.lee@gmail.com>
2023-09-12 02:51:54 +0900

    :sparkles: 서버 헤더 추가

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,9 +1,12 @@
+import { ChannelType } from '@prisma/client';
 import { redirect } from 'next/navigation';
 
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
+
+import { ServerHeader } from './server-header';
 
 type ServerSidebarProps = {
   serverId: string;
@@ -33,14 +36,23 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
       },
     },
   });
+
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
+  const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
+  const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
+
+  const memberrs = server?.members.filter((member) => member.profileId !== profile.id);
+
   if (!server) return redirect('/');
+
+  const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
   return (
     <nav
       className="flex-col flex-grow-0 flex-shrink-0 hidden h-full overflow-hidden bg-background-secondary md:flex w-240pxr text-basic basis-auto"
       aria-label={`${server.name} (서버)`}
     >
-      <div>Server Header</div>
+      <ServerHeader server={server} role={role} />
 
       <ScrollArea className="flex-1 px-12pxr">
         <div className="mt-8pxr">Server Search</div>
